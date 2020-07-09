657dc0c08553bca5234a79ab2207ca3a9ae853e3
cesar <cesar.ortran@gmail.com>
2020-07-09 01:09:17 -0500

    CodeIgniter Implementation as backend

---
+++ codeigniter/app/Controllers/Utilities.php
@@ -0,0 +1,18 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Utilities extends BaseController
+{
+	public function getToken()
+	{
+        $csrfData[] = [
+            'name_csrf' => csrf_token(),
+            'hash' => csrf_hash()
+        ];
+
+        echo json_encode($csrfData);
+	}
+
+
+}
